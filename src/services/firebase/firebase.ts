6d59e2cd08bbe1d27a7bb7b7f07b0fb821015073
Daniel Godin <DanielEGodin@gmail.com>
2025-10-01 11:32:04 -0700

    Cleared out code I wouldn't be using

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -3,15 +3,11 @@ import { firebaseConfig } from "./firebaseConfig";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 // import { devLog } from "../utils/logger";
-// import { getStorage, connectStorageEmulator } from "firebase/storage";
-// import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-// export const storage = getStorage(app);
-// export const functions = getFunctions(app);
 
 // Check if should use emulators
 const useEmulators = import.meta.env.VITE_USE_FIREBASE_EMULATORS === 'true';
@@ -21,6 +17,4 @@ if (useEmulators) {
     // devLog('Using Firebase Emulators');
     connectAuthEmulator(auth, "http://127.0.0.1:9099")
     connectFirestoreEmulator(db, '127.0.0.1', 8080)
-    // connectStorageEmulator(storage, "127.0.0.1", 9199);
-    // connectFunctionsEmulator(functions, 'localhost', 5001);
 }
