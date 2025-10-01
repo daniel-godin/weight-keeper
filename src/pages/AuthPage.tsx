c5686c4a25f62f7d537844d991286861cceaf528
Daniel Godin <DanielEGodin@gmail.com>
2025-10-01 12:58:36 -0700

    Basic setup for signing in. Missing handleSubmit logic and better styling

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,8 +1,48 @@
+import { useState, type FormEvent } from "react"
 
 
 export function AuthPage() {
+    const [formData, setFormData] = useState({ email: '', password: '' });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        console.log("handle submit triggered");
+    }
 
     return (
-        <>AuthPage</>
+        <main>
+            <header>Sign In</header>
+            <form onSubmit={handleSubmit}>
+                <label>Email:
+                    <input
+                        type="email"
+                        name="email"
+                        required={true}
+                        value={formData.email}
+                        onChange={handleInputChange}
+                    />
+                </label>
+
+                <label>Password:
+                    <input
+                        type="password"
+                        name="password"
+                        required={true}
+                        value={formData.password}
+                        onChange={handleInputChange}
+                    />
+                </label>
+            </form>
+        </main>
     )
 }
