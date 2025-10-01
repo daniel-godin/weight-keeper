ba861658dd2056987b187339e5447c2e3dd918e0
Daniel Godin <DanielEGodin@gmail.com>
2025-10-01 11:33:20 -0700

    Added a log in dev mode to indicate that I am using the emulators

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseConfig";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
-// import { devLog } from "../utils/logger";
+import { devLog } from "../../utils/logger"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -14,7 +14,7 @@ const useEmulators = import.meta.env.VITE_USE_FIREBASE_EMULATORS === 'true';
 
 // Connect to emulators if the flag is set
 if (useEmulators) {
-    // devLog('Using Firebase Emulators');
+    devLog('Using Firebase Emulators');
     connectAuthEmulator(auth, "http://127.0.0.1:9099")
     connectFirestoreEmulator(db, '127.0.0.1', 8080)
 }
