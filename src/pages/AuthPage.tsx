7595f2321b70a1dcaa086d8dc038882348eed062
Daniel Godin <DanielEGodin@gmail.com>
2025-10-01 14:01:50 -0700

    AuthPage correctly signing in

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,8 +1,16 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState, type FormEvent } from "react"
+import { auth } from "../services/firebase/firebase";
+import { devError, devLog } from "../utils/logger";
+import { useNavigate } from "react-router";
 
 
 export function AuthPage() {
+    const navigate = useNavigate();
+
     const [formData, setFormData] = useState({ email: '', password: '' });
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+    const [status, setStatus] = useState<string>('');
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -13,15 +21,40 @@ export function AuthPage() {
         }));
     }
 
-    const handleSubmit = (e: FormEvent) => {
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
 
-        console.log("handle submit triggered");
+        try {
+            setIsSubmitting(true);
+            setStatus("Signing In...");
+
+            const result = await signInWithEmailAndPassword(auth, formData.email.trim(), formData.password.trim());
+
+            devLog(result);
+
+            setStatus(`Welcome back, ${result.user.displayName}. Taking you to your home page now...`)
+
+            setTimeout(() => {
+                resetState();
+                navigate('/app');
+            }, 1000)
+
+        } catch (error) {
+            devError("Error in handleSubmit. Error: ", error);
+            setStatus("Error signing in.  Please try again");
+            setIsSubmitting(false);
+        }
+    }
+
+    const resetState = () => {
+        setFormData({ email: '', password: ''});
+        setIsSubmitting(false);
+        setStatus('');
     }
 
     return (
         <main>
-            <header>Sign In</header>
+            <header className="text-4xl font-bold">Sign In</header>
             <form onSubmit={handleSubmit}>
                 <label>Email:
                     <input
@@ -30,6 +63,7 @@ export function AuthPage() {
                         required={true}
                         value={formData.email}
                         onChange={handleInputChange}
+                        disabled={isSubmitting}
                     />
                 </label>
 
@@ -40,8 +74,13 @@ export function AuthPage() {
                         required={true}
                         value={formData.password}
                         onChange={handleInputChange}
+                        disabled={isSubmitting}
                     />
                 </label>
+
+                <output>{status}</output>
+
+                <button type="submit" disabled={isSubmitting}>Sign In</button>
             </form>
         </main>
     )
