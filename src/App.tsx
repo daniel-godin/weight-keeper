1f976aaaac95ca3a8d1dfccb81469400039eb320
Daniel Godin <DanielEGodin@gmail.com>
2025-10-01 12:58:50 -0700

    Rough Routes set up. Redirect set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { useAuth } from "./contexts/AuthContext";
+import { LandingPage } from "./pages/LandingPage";
+import { AppLayout } from "./layouts/AppLayout";
+import { AuthPage } from "./pages/AuthPage";
 
 export function App() {
     return (
-        <>App</>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/auth" element={<AuthPage />} />
+
+                {/* Catch-all Redirect */}
+                <Route path="*" element={<NotFoundRedirect />} />
+            </Routes>
+        </BrowserRouter>
     )
+}
+
+function ProtectedRoute () {
+    const { authState } = useAuth();
+
+    if (authState.loading) { return <>Loading...</> }; // TODO: Replace with proper loading component.
+    if (!authState.user) { return <Navigate to="/auth" replace /> }; // Auth Guard Clause.
+
+    // Add a DataProvider here later.
+    return (
+        <AppLayout />
+    )
+}
+
+function NotFoundRedirect() {
+    const { authState } = useAuth();
+
+    // If Logged In. Send to App page.
+    if (authState.user) { return <Navigate to="/app" replace /> };
+
+    // If not logged in, send to landing page
+    return <Navigate to="/" replace />
 }
